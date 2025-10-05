ac331e8bad010ff98578b8649d8b74cce2af2c67
boybaby2 <ezechinonso717@gmail.com>
2025-10-05 04:38:31 +0100

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
   const { url } = req.query;
   if (!url) {
     return res.status(400).json({ error: 'Missing ?url parameter' });
@@ -16,4 +16,4 @@ export default async function handler(req, res) {
     console.error("Proxy error:", error);
     res.status(500).json({ error: "Failed to fetch target URL" });
   }
-}
+};
